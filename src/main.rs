0a19245586b19ad803f72f513c561036bb7b25ca
senseisamy <samy.naji2003@gmail.com>
2026-03-06 18:30:24 +0100

    idk this is hard

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,17 @@ extern crate nx;
 
 extern crate alloc;
 
-use nx::result::*;
-use nx::util;
-use nx::svc;
 use nx::diag::abort;
 use nx::diag::log;
+use nx::ipc::client::IClientObject;
+use nx::ipc::sf;
+use nx::ipc::sf::sm::IUserInterfaceClient;
+use nx::ipc::sf::Session;
+use nx::result::*;
+use nx::service::sm::ServiceName;
+use nx::service::sm::UserInterface;
+use nx::svc;
+use nx::util;
 
 use core::panic;
 
@@ -20,14 +26,20 @@ static mut CUSTOM_HEAP: [u8; CUSTOM_HEAP_LEN] = [0; CUSTOM_HEAP_LEN];
 
 #[no_mangle]
 pub fn initialize_heap(_hbl_heap: util::PointerAndSize) -> util::PointerAndSize {
-    unsafe {
-        util::PointerAndSize::new(CUSTOM_HEAP.as_mut_ptr(), CUSTOM_HEAP.len())
-    }
+    unsafe { util::PointerAndSize::new(CUSTOM_HEAP.as_mut_ptr(), CUSTOM_HEAP.len()) }
 }
 
 #[no_mangle]
 pub fn main() -> Result<()> {
-    diag_log!(log::lm::LmLogger { log::LogSeverity::Trace, false } => "Hello world!");
+    diag_log!(log::lm::LmLogger { log::LogSeverity::Trace, false } => "Earpods driver starting...");
+
+    let sm_handle = unsafe { svc::connect_to_named_port(c"sm:") }?;
+    let session = Session::from_handle(sm_handle);
+    let user_interface = UserInterface::new(session);
+    user_interface.register_client(sf::ProcessId::new())?;
+    let usb_hs_handle = user_interface.get_service_handle(ServiceName::new("usb:hs"));
+
+    diag_log!(log::lm::LmLogger { log::LogSeverity::Trace, false } => "Successfully acquired the raw usb:hs session!");
 
     loop {
         // Sleep 10ms (aka 10'000'000 ns)
